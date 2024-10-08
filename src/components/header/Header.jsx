f6875c4aee884f7995edea7c5ac52cd55eb5a39e
Dimon11k <dmytroleskiv77@gmail.com>
2024-10-09 02:41:38 +0300

    Add styles for header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+import "../../styles/components/header/Header.scss";
+
 const Header = () => {
   return (
     <header className="header">
