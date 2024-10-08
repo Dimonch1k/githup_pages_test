e6f635b1b939608607c46258147077dec314aeef
Dimon11k <dmytroleskiv77@gmail.com>
2024-10-09 01:52:06 +0300

    try to connect github pages to have access to web-site from everywhere

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,15 @@ import { pageStyles, outletStyles } from "./styles/appStyles";
 
 function App() {
   return (
-    <div style={pageStyles}>
-      <ScrollRestoration />
-      <Header />
-      <div style={outletStyles}>
-        <Outlet />
-      </div>
+    // <div style={pageStyles}>
+    //   <ScrollRestoration />
+    //   <Header />
+    //   <div style={outletStyles}>
+    //     <Outlet />
+    //   </div>
+    // </div>
+    <div>
+      <h1>App Page</h1>
     </div>
   );
 }
