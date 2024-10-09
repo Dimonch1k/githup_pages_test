f98cdb4e177368c8eda3fec01acc58079aae9935
Dimon11k <dmytroleskiv77@gmail.com>
2024-10-09 03:25:28 +0300

    Try to to better styles for body for every page and try to make header that hide on scroll down and shows on scroll up

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
 import "../../styles/components/header/Header.scss";
 
 const Header = () => {
+  const [isVisible, setIsVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const controlHeader = () => {
+    if (typeof window !== "undefined") {
+      if (window.screenY)
+        if (window.scrollY > lastScrollY) setIsVisible(false); // Scrolling down
+        else {
+          setIsVisible(true); // Scrolling up
+        }
+      setLastScrollY(window.scrollY); // Update last scroll position
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", controlHeader);
+    return () => {
+      window.removeEventListener("scroll", controlHeader); // Cleanup the event listener
+    };
+  }, [lastScrollY]);
+
   return (
-    <header className="header">
+    <header
+      className="header"
+      style={{
+        transform: isVisible ? "translateY(0)" : "translateY(-100%)",
+        transition: "transform 0.3s ease",
+      }}
+    >
       <div className="content">
         <nav>
           <NavLink to="/">Home</NavLink>
--- src/styles/appStyles.js
+++ src/styles/appStyles.js
@@ -7,8 +7,4 @@ export const pageStyles = {
 export const outletStyles = {
   flex: 1,
   overflow: "auto",
-  padding: "20px 50px 50px",
-  maxWidth: 1560,
-  width: "100%",
-  margin: "0 auto",
 };
