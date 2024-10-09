d8c7ec34a42a164090c653d41ecf26393512cae4
Dimon11k <dmytroleskiv77@gmail.com>
2024-10-09 03:15:16 +0300

    Try to to better styles for body for every page

--- src/styles/appStyles.js
+++ src/styles/appStyles.js
@@ -7,4 +7,8 @@ export const pageStyles = {
 export const outletStyles = {
   flex: 1,
   overflow: "auto",
+  padding: "20px 50px 50px",
+  maxWidth: 1560,
+  width: "100%",
+  margin: "0 auto",
 };
